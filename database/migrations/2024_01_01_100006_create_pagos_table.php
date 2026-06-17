ad18e1a33442e72500e5096061c7ce9e4f516947
uneven74 <ahtnezev@gmail.com>
2026-06-17 16:38:30 -0600

    [F] update migration type field

--- database/migrations/2024_01_01_100006_create_pagos_table.php
+++ database/migrations/2024_01_01_100006_create_pagos_table.php
@@ -12,8 +12,8 @@ return new class extends Migration
             $table->id();
             $table->string('matricula');
             $table->text('concepto');
-            $table->text('monto');
-            $table->text('fecha_pago');
+            $table->decimal('monto', 11, 2);
+            $table->date('fecha_pago');
             $table->text('metodo')->nullable();
             $table->integer('sede_id')->nullable();
             $table->integer('conciliacion_id')->nullable();
